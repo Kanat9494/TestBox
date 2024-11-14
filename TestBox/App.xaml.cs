77c5eb56c7bdc3cb075da358eeb97e450c882453
kana2022 <kkanat94@gmail.com>
2024-11-14 13:07:51 +0600

    Add project files.

---
+++ TestBox/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TestBox
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
